fa6e351abb2e16a80351dcedb946c00afbbd196f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-13 12:32:32 +0900

    index

--- chemicaljson.py
+++ chemicaljson.py
@@ -41,7 +41,7 @@ class Connections(BaseModel):
     Length must be the number of bonds * 2
     """
 
-    index: List[NonNegativeInt]
+    index: List[NonNegativeInt] = Field(..., description="zero-based numbering.")
 
 
 class Bonds(BaseModel):
